9ef0867a6fd3a662ab8898c6c1a7f7cbadaf4549
David Millington <vintagedave@gmail.com>
2024-10-27 19:30:56 +0200

    Could not resolve exceptions. PHP requires each class be in a file with the class name, including folder structure

---
+++ src/Exceptions/BadRequestException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Respectify\Exceptions;
+
+use Respectify\Exceptions\RespectifyException;
+
+class BadRequestException extends RespectifyException {
+}
---
+++ src/Exceptions/RespectifyException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Respectify\Exceptions;
+
+use Exception;
+
+class RespectifyException extends \Exception {}
+
