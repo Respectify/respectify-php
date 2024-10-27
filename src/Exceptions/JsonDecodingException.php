26482bf9df683125220601c3272fddffe50e1f97
David Millington <vintagedave@gmail.com>
2024-10-27 19:30:56 +0200

    Could not resolve exceptions. PHP requires each class be in a file with the class name, including folder structure

---
+++ src/Exceptions/JsonDecodingException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Respectify\Exceptions;
+
+use Respectify\Exceptions\RespectifyException;
+
+class JsonDecodingException extends RespectifyException {
+}
+
---
+++ src/Exceptions/UnsupportedMediaTypeException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Respectify\Exceptions;
+
+use Respectify\Exceptions\RespectifyException;
+
+class UnsupportedMediaTypeException extends RespectifyException {
+}
