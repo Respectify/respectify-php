10f13b9fb69c8fdc8f658fb3688ec8006a089e8d
David Millington <vintagedave@gmail.com>
2024-10-27 17:50:56 +0200

    Tests now running, though failing

--- tests/RespectifyClientAsyncTests.php
+++ tests/RespectifyClientAsyncTests.php
@@ -3,27 +3,33 @@
 use PHPUnit\Framework\TestCase;
 use Mockery as m;
 use Respectify\RespectifyClientAsync;
+use Respectify\CommentScore;
 use Respectify\Exceptions\BadRequestException;
 use Respectify\Exceptions\UnauthorizedException;
 use Respectify\Exceptions\UnsupportedMediaTypeException;
 use Respectify\Exceptions\JsonDecodingException;
-use Respectify\Exceptions\ArticleIdNotFoundException;
 use Respectify\Exceptions\RespectifyException;
 use React\Http\Browser;
-use React\EventLoop\Factory;
+use React\EventLoop\Loop;
 use Psr\Http\Message\ResponseInterface;
 use React\Promise\PromiseInterface;
+use function React\Promise\resolve;
 
-class RespectifyClientAsyncTest extends TestCase {
+class RespectifyClientAsyncTests extends TestCase {
     private $client;
     private $browserMock;
     private $loop;
 
     protected function setUp(): void {
-        $this->loop = Factory::create();
+        $this->loop = Loop::get();
         $this->browserMock = m::mock(Browser::class);
         $this->client = new RespectifyClientAsync('test@example.com', 'testapikey');
-        $this->client = $this->browserMock;
+        
+        // Use reflection to set the private $client property
+        $reflection = new \ReflectionClass($this->client);
+        $clientProperty = $reflection->getProperty('client');
+        $clientProperty->setAccessible(true);
+        $clientProperty->setValue($this->client, $this->browserMock);
     }
 
     protected function tearDown(): void {
@@ -35,7 +41,7 @@ class RespectifyClientAsyncTest extends TestCase {
         $responseMock->shouldReceive('getStatusCode')->andReturn(200);
         $responseMock->shouldReceive('getBody')->andReturn(json_encode(['article_id' => '1234']));
 
-        $this->browserMock->shouldReceive('post')->andReturn(new \React\Promise\FulfilledPromise($responseMock));
+        $this->browserMock->shouldReceive('post')->andReturn(resolve($responseMock));
 
         $promise = $this->client->initTopicFromText('Sample text');
         $promise->then(function ($articleId) {
@@ -45,155 +51,161 @@ class RespectifyClientAsyncTest extends TestCase {
         $this->client->run();
     }
 
-    public function testInitTopicFromTextMissingArticleId() {
+    public function testInitTopicGivingTextMissingArticleId() {
         $this->expectException(JsonDecodingException::class);
 
         $responseMock = m::mock(ResponseInterface::class);
         $responseMock->shouldReceive('getStatusCode')->andReturn(200);
-        $responseMock->shouldReceive('getBody')->andReturn(json_encode([]));
+        $responseMock->shouldReceive('getBody')->andReturn(json_encode([])); // Mock empty JSON, so missing article_id
 
-        $this->browserMock->shouldReceive('post')->andReturn(new \React\Promise\FulfilledPromise($responseMock));
-
-        $promise = $this->client->initTopicFromText('Sample text');
-        $promise->otherwise(function ($e) {
-            throw $e;
-        });
-
-        $this->client->run();
-    }
-
-    public function testInitTopicFromTextBadRequest() {
-        $this->expectException(BadRequestException::class);
-
-        $responseMock = m::mock(ResponseInterface::class);
-        $responseMock->shouldReceive('getStatusCode')->andReturn(400);
-        $responseMock->shouldReceive('getReasonPhrase')->andReturn('Bad Request');
-
-        $this->browserMock->shouldReceive('post')->andReturn(new \React\Promise\FulfilledPromise($responseMock));
+        $this->browserMock->shouldReceive('post')->andReturn(resolve($responseMock));
 
         $promise = $this->client->initTopicFromText('Sample text');
-        $promise->otherwise(function ($e) {
-            throw $e;
-        });
+        $promise->then(
+            function ($articleId) {
+                // This should not be called
+                $this->fail('Expected exception not thrown');
+            },
+            function ($e) {
+                throw $e;
+            }
+        );
 
         $this->client->run();
     }
 
-    public function testInitTopicFromUrlSuccess() {
-        $responseMock = m::mock(ResponseInterface::class);
-        $responseMock->shouldReceive('getStatusCode')->andReturn(200);
-        $responseMock->shouldReceive('getBody')->andReturn(json_encode(['article_id' => '1234']));
+    // public function testInitTopicFromTextBadRequest() {
+    //     $this->expectException(BadRequestException::class);
 
-        $this->browserMock->shouldReceive('post')->andReturn(new \React\Promise\FulfilledPromise($responseMock));
+    //     $responseMock = m::mock(ResponseInterface::class);
+    //     $responseMock->shouldReceive('getStatusCode')->andReturn(400);
+    //     $responseMock->shouldReceive('getReasonPhrase')->andReturn('Bad Request');
 
-        $promise = $this->client->initTopicFromUrl('https://example.com');
-        $promise->then(function ($articleId) {
-            $this->assertEquals('1234', $articleId);
-        });
+    //     $this->browserMock->shouldReceive('post')->andReturn(resolve($responseMock));
 
-        $this->client->run();
-    }
+    //     $promise = $this->client->initTopicFromText('Sample text');
+    //     $promise->otherwise(function ($e) {
+    //         throw $e;
+    //     });
 
-    public function testInitTopicFromUrlBadRequest() {
-        $this->expectException(BadRequestException::class);
+    //     $this->client->run();
+    // }
 
-        $responseMock = m::mock(ResponseInterface::class);
-        $responseMock->shouldReceive('getStatusCode')->andReturn(400);
-        $responseMock->shouldReceive('getReasonPhrase')->andReturn('Bad Request');
+    // public function testInitTopicFromUrlSuccess() {
+    //     $responseMock = m::mock(ResponseInterface::class);
+    //     $responseMock->shouldReceive('getStatusCode')->andReturn(200);
+    //     $responseMock->shouldReceive('getBody')->andReturn(json_encode(['article_id' => '1234']));
 
-        $this->browserMock->shouldReceive('post')->andReturn(new \React\Promise\FulfilledPromise($responseMock));
+    //     $this->browserMock->shouldReceive('post')->andReturn(resolve($responseMock));
 
-        $promise = $this->client->initTopicFromUrl('https://example.com');
-        $promise->otherwise(function ($e) {
-            throw $e;
-        });
+    //     $promise = $this->client->initTopicFromUrl('https://example.com');
+    //     $promise->then(function ($articleId) {
+    //         $this->assertEquals('1234', $articleId);
+    //     });
 
-        $this->client->run();
-    }
+    //     $this->client->run();
+    // }
 
-    public function testEvaluateCommentSuccess() {
-        $responseMock = m::mock(ResponseInterface::class);
-        $responseMock->shouldReceive('getStatusCode')->andReturn(200);
-        $responseMock->shouldReceive('getBody')->andReturn(json_encode([
-            'logical_fallacies' => [],
-            'objectionable_phrases' => [],
-            'negative_tone_phrases' => [],
-            'appears_low_effort' => false,
-            'is_spam' => false,
-            'overall_score' => 5
-        ]));
+    // public function testInitTopicFromUrlBadRequest() {
+    //     $this->expectException(BadRequestException::class);
 
-        $this->browserMock->shouldReceive('post')->andReturn(new \React\Promise\FulfilledPromise($responseMock));
+    //     $responseMock = m::mock(ResponseInterface::class);
+    //     $responseMock->shouldReceive('getStatusCode')->andReturn(400);
+    //     $responseMock->shouldReceive('getReasonPhrase')->andReturn('Bad Request');
 
-        $promise = $this->client->evaluateComment([
-            'article_context_id' => '2b38cb34-e3d7-492e-b61e-c3858f1863b7',
-            'comment' => 'This is a test comment'
-        ]);
-        $promise->then(function ($commentScore) {
-            $this->assertInstanceOf(CommentScore::class, $commentScore);
-            $this->assertEquals(5, $commentScore->overallScore);
-        });
+    //     $this->browserMock->shouldReceive('post')->andReturn(resolve($responseMock));
 
-        $this->client->run();
-    }
+    //     $promise = $this->client->initTopicFromUrl('https://example.com');
+    //     $promise->otherwise(function ($e) {
+    //         throw $e;
+    //     });
 
-    public function testEvaluateCommentBadRequest() {
-        $this->expectException(BadRequestException::class);
+    //     $this->client->run();
+    // }
 
-        $responseMock = m::mock(ResponseInterface::class);
-        $responseMock->shouldReceive('getStatusCode')->andReturn(400);
-        $responseMock->shouldReceive('getReasonPhrase')->andReturn('Bad Request');
+    // public function testEvaluateCommentSuccess() {
+    //     $responseMock = m::mock(ResponseInterface::class);
+    //     $responseMock->shouldReceive('getStatusCode')->andReturn(200);
+    //     $responseMock->shouldReceive('getBody')->andReturn(json_encode([
+    //         'logical_fallacies' => [],
+    //         'objectionable_phrases' => [],
+    //         'negative_tone_phrases' => [],
+    //         'appears_low_effort' => false,
+    //         'is_spam' => false,
+    //         'overall_score' => 5
+    //     ]));
 
-        $this->browserMock->shouldReceive('post')->andReturn(new \React\Promise\FulfilledPromise($responseMock));
+    //     $this->browserMock->shouldReceive('post')->andReturn(resolve($responseMock));
 
-        $promise = $this->client->evaluateComment([
-            'article_context_id' => '2b38cb34-e3d7-492e-b61e-c3858f1863b7',
-            'comment' => 'This is a test comment'
-        ]);
-        $promise->otherwise(function ($e) {
-            throw $e;
-        });
+    //     $promise = $this->client->evaluateComment([
+    //         'article_context_id' => '2b38cb34-e3d7-492e-b61e-c3858f1863b7',
+    //         'comment' => 'This is a test comment'
+    //     ]);
+    //     $promise->then(function ($commentScore) {
+    //         $this->assertInstanceOf(CommentScore::class, $commentScore);
+    //         $this->assertEquals(5, $commentScore->overallScore);
+    //     });
 
-        $this->client->run();
-    }
+    //     $this->client->run();
+    // }
 
-    public function testEvaluateCommentUnauthorized() {
-        $this->expectException(UnauthorizedException::class);
+    // public function testEvaluateCommentBadRequest() {
+    //     $this->expectException(BadRequestException::class);
 
-        $responseMock = m::mock(ResponseInterface::class);
-        $responseMock->shouldReceive('getStatusCode')->andReturn(401);
-        $responseMock->shouldReceive('getReasonPhrase')->andReturn('Unauthorized');
+    //     $responseMock = m::mock(ResponseInterface::class);
+    //     $responseMock->shouldReceive('getStatusCode')->andReturn(400);
+    //     $responseMock->shouldReceive('getReasonPhrase')->andReturn('Bad Request');
 
-        $this->browserMock->shouldReceive('post')->andReturn(new \React\Promise\FulfilledPromise($responseMock));
+    //     $this->browserMock->shouldReceive('post')->andReturn(resolve($responseMock));
 
-        $promise = $this->client->evaluateComment([
-            'article_context_id' => '2b38cb34-e3d7-492e-b61e-c3858f1863b7',
-            'comment' => 'This is a test comment'
-        ]);
-        $promise->otherwise(function ($e) {
-            throw $e;
-        });
+    //     $promise = $this->client->evaluateComment([
+    //         'article_context_id' => '2b38cb34-e3d7-492e-b61e-c3858f1863b7',
+    //         'comment' => 'This is a test comment'
+    //     ]);
+    //     $promise->otherwise(function ($e) {
+    //         throw $e;
+    //     });
 
-        $this->client->run();
-    }
+    //     $this->client->run();
+    // }
 
-    public function testEvaluateCommentUnsupportedMediaType() {
-        $this->expectException(UnsupportedMediaTypeException::class);
+    // public function testEvaluateCommentUnauthorized() {
+    //     $this->expectException(UnauthorizedException::class);
 
-        $responseMock = m::mock(ResponseInterface::class);
-        $responseMock->shouldReceive('getStatusCode')->andReturn(415);
-        $responseMock->shouldReceive('getReasonPhrase')->andReturn('Unsupported Media Type');
+    //     $responseMock = m::mock(ResponseInterface::class);
+    //     $responseMock->shouldReceive('getStatusCode')->andReturn(401);
+    //     $responseMock->shouldReceive('getReasonPhrase')->andReturn('Unauthorized');
 
-        $this->browserMock->shouldReceive('post')->andReturn(new \React\Promise\FulfilledPromise($responseMock));
+    //     $this->browserMock->shouldReceive('post')->andReturn(resolve($responseMock));
 
-        $promise = $this->client->evaluateComment([
-            'article_context_id' => '2b38cb34-e3d7-492e-b61e-c3858f1863b7',
-            'comment' => 'This is a test comment'
-        ]);
-        $promise->otherwise(function ($e) {
-            throw $e;
-        });
+    //     $promise = $this->client->evaluateComment([
+    //         'article_context_id' => '2b38cb34-e3d7-492e-b61e-c3858f1863b7',
+    //         'comment' => 'This is a test comment'
+    //     ]);
+    //     $promise->otherwise(function ($e) {
+    //         throw $e;
+    //     });
 
-        $this->client->run();
-    }
+    //     $this->client->run();
+    // }
+
+    // public function testEvaluateCommentUnsupportedMediaType() {
+    //     $this->expectException(UnsupportedMediaTypeException::class);
+
+    //     $responseMock = m::mock(ResponseInterface::class);
+    //     $responseMock->shouldReceive('getStatusCode')->andReturn(415);
+    //     $responseMock->shouldReceive('getReasonPhrase')->andReturn('Unsupported Media Type');
+
+    //     $this->browserMock->shouldReceive('post')->andReturn(resolve($responseMock));
+
+    //     $promise = $this->client->evaluateComment([
+    //         'article_context_id' => '2b38cb34-e3d7-492e-b61e-c3858f1863b7',
+    //         'comment' => 'This is a test comment'
+    //     ]);
+    //     $promise->otherwise(function ($e) {
+    //         throw $e;
+    //     });
+
+    //     $this->client->run();
+    // }
 }
