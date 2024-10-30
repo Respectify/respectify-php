752455ee43267d061f7f07f1a2a90cf36246854b
David Millington <vintagedave@gmail.com>
2024-10-30 22:08:08 +0200

    Much better documentation

--- src/Exceptions/BadRequestException.php
+++ src/Exceptions/BadRequestException.php
@@ -4,5 +4,10 @@ namespace Respectify\Exceptions;
 
 use Respectify\Exceptions\RespectifyException;
 
+/**
+ * Exception thrown when the API returns a 400 Bad Request status code.
+ * This can happen if the request does not contain the correct data: for example, if a required field is blank.
+ * One common example is when the text to be analyzed is empty: passing an empty string causes a bad request.
+ */
 class BadRequestException extends RespectifyException {
 }
--- src/Exceptions/JsonDecodingException.php
+++ src/Exceptions/JsonDecodingException.php
@@ -4,6 +4,10 @@ namespace Respectify\Exceptions;
 
 use Respectify\Exceptions\RespectifyException;
 
+/**
+ * Exception thrown when there is an error decoding JSON when parsing the Respectify API response.
+ * This may include missing fields if a specific one is expected, or invalid JSON.
+ */
 class JsonDecodingException extends RespectifyException {
 }
 
