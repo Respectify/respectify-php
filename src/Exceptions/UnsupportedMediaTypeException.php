cc4993e18d703f0ff9d5e95376359e481a096d6e
David Millington <dave@respectify.ai>
2026-04-08 01:09:20 +0300

    Remove obsolete test backup files and stale exception doc URL

--- src/Exceptions/UnsupportedMediaTypeException.php
+++ src/Exceptions/UnsupportedMediaTypeException.php
@@ -12,7 +12,6 @@ use Respectify\Exceptions\RespectifyException;
  * the API will return a 415 Unsupported Media Type status code because it has no way to parse that media type.
  * 
  * In practice this might mean a specific document type that the API does not support.
- * Check [the REST API documentation](http://localhost:3000/api/initialize-topic) for the supported media types.
  */
 class UnsupportedMediaTypeException extends RespectifyException {
 }
